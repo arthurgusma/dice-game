8a496cae40f5036f4054033797f3f349249481ef
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-07-20 22:41:55 -0300

    ok

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 /*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
+the only purpose of the documentatio is for my own learning process and
+has nothing to do with readability for others.
 */
+
+var scores, roundScore, activePlayer;
+
+scores = (0,0);
+roundScore = 0;
+activePlayer = 0;
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// .textContent = allows us to switch the content inside the object
+// HTML content inside JS needs to '' to be reconized as so
+
+document.querySelector('.dice').style.display = 'none';
+// querySelector allows to select an HTML element and change it or re-store it like so:
+// var x = document.querySelector('#current-' + activePlayer).textContent;
+// if I console.log(x), it shows the .dice content
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // Math.random generates a random number between 0 and 1.
+  // * 6 generates it between 0 and 5
+  // and adding 1 to the count makes interval 1 to 6
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    document.querySelector('#current-' + activePlayer).textContent = '0';
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    // if else
+
+    roundScore = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //document.querySelector('.player-0-panel').classList.remove('active')
+    //document.querySelector('.player-1-panel').classList.add('active')
+  }
+});
