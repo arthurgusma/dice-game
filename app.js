424d6849f5d1c437ad822c41a7c0eba82f57a110
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-07-22 12:46:00 -0300

    JS code added to the New Game function.

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ the only purpose of the documentatio is for my own learning process and
 has nothing to do with readability for others.
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+function initGame() {
+  gamePlaying = true;
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+}
 
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
+initGame();
+
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // .textContent = allows us to switch the content inside the object
 // HTML content inside JS needs to '' to be reconized as so
@@ -24,41 +35,45 @@ document.querySelector('.dice').style.display = 'none';
 
 //DICE ROLL BTN
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  var dice = Math.floor(Math.random() * 6) + 1;
-  // Math.random generates a random number between 0 and 1.
-  // * 6 generates it between 0 and 5
-  // and adding 1 to the count makes interval 1 to 6
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
+  if(gamePlaying) {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // Math.random generates a random number between 0 and 1.
+    // * 6 generates it between 0 and 5
+    // and adding 1 to the count makes interval 1 to 6
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-  if (dice !== 1) {
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    document.querySelector('#current-' + activePlayer).textContent = 0;
-    nextPlayer();
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      document.querySelector('#current-' + activePlayer).textContent = 0;
+      nextPlayer();
+    }
   }
-
 });
 
 // HOLD BTN
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  // add roundScore to score- activePlayer.
-  scores[activePlayer] += roundScore;
+  if(gamePlaying) {
+    // add roundScore to score- activePlayer.
+    scores[activePlayer] += roundScore;
 
-  document.querySelector('#current-' + activePlayer).textContent = 0;
-  document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    document.querySelector('#current-' + activePlayer).textContent = 0;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-  if (scores[activePlayer] >= 100) {
-    document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-    document.querySelector('.dice').style.display = 'none';
-    alert('Congrats! You\'ve won the game!')
-  } else {
-    nextPlayer();
+    if (scores[activePlayer] >= 100) {
+      document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+      document.querySelector('.dice').style.display = 'none';
+      alert('Congrats! You\'ve won the game!')
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
   }
 
-})
+});
 
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -67,6 +82,9 @@ function nextPlayer() {
   roundScore = 0;
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
-  //document.querySelector('.player-0-panel').classList.remove('active')
-  //document.querySelector('.player-1-panel').classList.add('active')
 }
+
+
+// NEW GAME function
+
+document.querySelector('.btn-new').addEventListener('click', initGame);
