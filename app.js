2db85174b107080802f0bd01bc690fb588c8d742
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-07-21 22:46:36 -0300

    JS code added to the HOLD BTN function.

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ has nothing to do with readability for others.
 
 var scores, roundScore, activePlayer;
 
-scores = (0,0);
+scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // .textContent = allows us to switch the content inside the object
 // HTML content inside JS needs to '' to be reconized as so
@@ -22,6 +22,7 @@ document.querySelector('.dice').style.display = 'none';
 // var x = document.querySelector('#current-' + activePlayer).textContent;
 // if I console.log(x), it shows the .dice content
 
+//DICE ROLL BTN
 document.querySelector('.btn-roll').addEventListener('click', function() {
   var dice = Math.floor(Math.random() * 6) + 1;
   // Math.random generates a random number between 0 and 1.
@@ -35,14 +36,37 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    document.querySelector('#current-' + activePlayer).textContent = '0';
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    // if else
-
-    roundScore = 0;
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    //document.querySelector('.player-0-panel').classList.remove('active')
-    //document.querySelector('.player-1-panel').classList.add('active')
+    document.querySelector('#current-' + activePlayer).textContent = 0;
+    nextPlayer();
   }
+
 });
+
+// HOLD BTN
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // add roundScore to score- activePlayer.
+  scores[activePlayer] += roundScore;
+
+  document.querySelector('#current-' + activePlayer).textContent = 0;
+  document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+    document.querySelector('.dice').style.display = 'none';
+    alert('Congrats! You\'ve won the game!')
+  } else {
+    nextPlayer();
+  }
+
+})
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  // if else
+
+  roundScore = 0;
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  //document.querySelector('.player-0-panel').classList.remove('active')
+  //document.querySelector('.player-1-panel').classList.add('active')
+}
